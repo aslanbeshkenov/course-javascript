510afef832b784e2b4947a1966b48a564b4ae668
Аслан Бешкенов <aslanbeshkenov@mail.ru>
2023-05-19 20:15:31 +0600

    dz 2 week

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,12 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el)); // выведет каждый элемент массива
  */
-function forEach() {}
+function forEach(arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    fn(el, i, arr);
+  }
+}
 
 /*
  Задание 2:
@@ -21,7 +26,15 @@ function forEach() {}
    const newArray = map([1, 2, 3], (el) => el ** 2);
    console.log(newArray); // выведет [1, 4, 9]
  */
-function map() {}
+function map(arr, fn) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+    const newEl = fn(el, i, arr);
+    newArr.push(newEl);
+  }
+  return newArr;
+}
 
 /*
  Задание 3:
@@ -33,7 +46,23 @@ function map() {}
    const sum = reduce([1, 2, 3], (all, current) => all + current);
    console.log(sum); // выведет 6
  */
-function reduce() {}
+function reduce(arr, fn, initialValue) {
+  let result = 0;
+  let startIndex = 0;
+
+  if (initialValue != undefined) {
+    result = initialValue;
+  } else {
+    result = arr[0];
+    startIndex = 1;
+  }
+
+  for (let i = startIndex; i < arr.length; i++) {
+    const el = arr[i];
+    result = fn(result, el, i, arr);
+  }
+  return result;
+}
 
 /*
  Задание 4:
@@ -44,6 +73,14 @@ function reduce() {}
    const keys = upperProps({ name: 'Сергей', lastName: 'Петров' });
    console.log(keys) // выведет ['NAME', 'LASTNAME']
  */
-function upperProps() {}
+function upperProps(obj) {
+  const arr = Object.keys(obj);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const upperString = arr[i].toUpperCase();
+    newArr.push(upperString);
+  }
+  return newArr;
+}
 
 export { forEach, map, reduce, upperProps };
